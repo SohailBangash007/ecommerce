ca1cfd2290b92d67f7162c8ca00b86fb0a7fbcaf
SohailBangash007 <sohailbangash845@gmail.com>
2026-04-03 15:34:36 +0500

    navbar and hero section is update

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-       This  is  Home  Page
+       
+    <Hero />
+
     </div>
   )
 }
