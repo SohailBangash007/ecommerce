e71479203ca72196202a3804f0adba2d38269256
SohailBangash007 <sohailbangash845@gmail.com>
2026-04-05 12:41:09 +0500

    Collection page update

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-            <img src={assets.logo} alt="" className='mb-5 w-32'/>
-            <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iure et voluptates quisquam consequatur reiciendis ad ut. 
-            </p>
+          <img src={assets.logo} alt="" className="mb-5 w-32" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iure
+            et voluptates quisquam consequatur reiciendis ad ut.
+          </p>
         </div>
-      <div>
-        <p className='text-xl font-medium mb-5'>COMPANY</p>
-        <ul className='flex flex-col text-gray-600 gap-1'>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Delivery</li>
-          <li>Privacy Policy</li>
-        </ul>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col text-gray-600 gap-1">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col text-gray-600 gap-1">
+            <li>+1-212-456-7890</li>
+            <li>contact@foreveryou.com</li>
+          </ul>
+        </div>
       </div>
 
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2026@ forever.com - All Right Reserved.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- front-end/src/pages/Collection.jsx
+++ front-end/src/pages/Collection.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import {ShopContext} from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const Collection = () => {
+
+const { products } = useContext(ShopContext);
+const [showFilter,setShowFilter] = useState(false);
+
+
   return (
-    <div>
-      This is Collection Page
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+     {/* Filter Option */}
+     <div className='min-w-60'>
+      <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+
+        <img src={assets.dropdown_icon} alt="" className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`}/>
+      </p>
+       {/* Category Filter */}
+       <div className={` border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block `}>
+        <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+        <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+
+          <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Men'} /> Men
+          </p>
+
+           <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Women'} /> Women
+          </p>
+
+           <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Kids'} /> Kids
+          </p>
+        </div>
+       </div>
+
+      {/* SubCategory Filter */}
+      
+      <div className={` border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block `}>
+        <p className='mb-3 text-sm font-medium'>TYPE</p>
+        <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+
+          <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Top Wear'} /> Top Wear
+          </p>
+
+           <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Bottom Wear'} /> Bottom Wear
+          </p>
+
+           <p className='flex gap-2'>
+           <input className='w-3' type="checkbox" value={'Winter WEAR'} /> Winter WEAR
+          </p>
+        </div>
+       </div>
+      
+
+     </div>
+
     </div>
   )
 }
